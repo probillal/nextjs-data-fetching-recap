bf63c5b6275c7a7450241f6835c53aa1145ea802
probillal <smbillalhossain273@gmail.com>
2026-06-16 23:02:41 +0600

    Practice 3 types of fetching data in server

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+// const getPosts = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   return res.json();
+// };
 
-const PostPage = () => {
+const getPosts = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return res.json();
+  } catch (error) {
+    throw new Error("Error fetching posts:", error);
+    return [];
+  }
+};
+const PostPage = async () => {
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //   const posts = await res.json();
+  const posts = await getPosts();
   return (
     <div>
-      <h2>This is the Post Page</h2>
+      <h2>This is the Post Page: {posts.length}</h2>
     </div>
   );
 };
